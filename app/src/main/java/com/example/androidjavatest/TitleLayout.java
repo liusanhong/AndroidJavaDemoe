8e8b62e1922b90db338849abe27f6faca2f9c891
qiang.liu <qiang.liu@wuage.com>
2020-08-24 18:33:50 +0800

    自定义title layout

---
+++ app/src/main/java/com/example/androidjavatest/TitleLayout.java
@@ -0,0 +1,46 @@
+package com.example.androidjavatest;
+
+import android.app.Activity;
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import androidx.annotation.Nullable;
+
+/**
+ * @Description: java类作用描述
+ * @Author: LQ
+ * @CreateDate: 2020/8/24 5:45 PM
+ */
+public class TitleLayout extends LinearLayout {
+
+    public TitleLayout(final Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        LayoutInflater.from(context).inflate(R.layout.title,this);
+        Button back = (Button) findViewById(R.id.title_back);
+        back.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                ((Activity)getContext()).finish();
+            }
+        });
+
+        TextView titleText = (TextView) findViewById(R.id.title_text);
+
+        Button titleEdit = (Button) findViewById(R.id.title_edit);
+        titleEdit.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Toast.makeText(getContext(),"Click Edit",Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+
+    }
+}
